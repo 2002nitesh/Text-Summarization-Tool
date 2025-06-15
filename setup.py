da82cfb87198443ed48932ff46e2a86396451ad6
2002nitesh <kumarkumawatnitesh886@gmail.com>
2025-06-15 08:52:29 +0530

    Final commit- Build the complete project and GUI using fast api

--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
+
 __version__ = "0.0.0"
 
 REPO_NAME = "Text-Summarization-Tool"
 AUTHOR_USER_NAME = "2002nitesh"
 SRC_REPO = "TextSummarizationTool"
-AUTHOR_EMAIL = "kumarkumawatnitesh886@gmail.com"    
+AUTHOR_EMAIL = "kumarkumawatnitesh886@gmail.com" 
+
+
 
 setuptools.setup(
     name=SRC_REPO,
@@ -23,5 +26,5 @@ setuptools.setup(
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    packages=setuptools.find_packages(where="src")
 )
